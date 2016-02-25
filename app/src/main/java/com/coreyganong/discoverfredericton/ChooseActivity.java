188ea14b0d1c41e465d31179bac359d0a5abcbef
coreyganong <coreyganong@gmail.com>
2016-02-25 14:56:51 -0400

    Added toolbar

--- app/src/main/java/com/coreyganong/discoverfredericton/ChooseActivity.java
+++ app/src/main/java/com/coreyganong/discoverfredericton/ChooseActivity.java
@@ -7,13 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toolbar;
 
 public class ChooseActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setActionBar(toolbar);
         Button tourButton = (Button) findViewById(R.id.tour_btn);
         tourButton.setOnClickListener(new View.OnClickListener() {
             @Override
